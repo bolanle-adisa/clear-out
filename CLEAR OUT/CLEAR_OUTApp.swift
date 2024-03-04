2a8e078191b73402c55f89e2c7dc61485d0d6595
Bolanle Adisa <bolanleadisa6718@gmail.com>
2024-03-04 15:55:01 -0600

    Initial Commit

---
+++ CLEAR OUT/CLEAR_OUTApp.swift
@@ -0,0 +1,17 @@
+//
+//  CLEAR_OUTApp.swift
+//  CLEAR OUT
+//
+//  Created by Bolanle Adisa on 3/4/24.
+//
+
+import SwiftUI
+
+@main
+struct CLEAR_OUTApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ CLEAR OUT/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  CLEAR OUT
+//
+//  Created by Bolanle Adisa on 3/4/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
