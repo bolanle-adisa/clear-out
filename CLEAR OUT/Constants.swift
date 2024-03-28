4d567965c7fb423bd0c565d8754d0228967756b6
Bolanle Adisa <bolanleadisa6718@gmail.com>
2024-03-27 20:58:24 -0500

    help center complete

---
+++ CLEAR OUT/Constants.swift
@@ -0,0 +1,15 @@
+////
+////  Constants.swift
+////  CLEAR OUT
+////
+////  Created by Bolanle Adisa on 3/27/24.
+////
+//
+//import Foundation
+//
+//enum Constants {
+//    static let email = "mailto:graceekolawole@gmail.com"
+//    static let instagram = "https://www.instagram.com/oluw.anifemigrace?igsh=MWo4Mm1uOTl2NzVxdQ%3D%3D&utm_source=qr"
+//    static let privacy = "https://docs.google.com/document/d/1dxVvJk4LGTDpVzPv-rk7OXUpRFM5H_vRXqa4DXIqcjY/edit?usp=sharing"
+//   // static let terms = "https://docs.google.com/document/d/1q9FDlNP3W1MXRAQAmcvmwbyWbhwNCQFBxA9G7MuSU9s/edit?usp=sharing"
+//}
